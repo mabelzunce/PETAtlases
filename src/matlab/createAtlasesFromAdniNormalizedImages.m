e93058511c010a8002102a1a9c1e9933f5b9da61
Martin Belzunce <martin.a.belzunce@gmail.com>
2023-07-27 19:21:35 -0300

    Updated the names and paths for the PETQueantificationTools, finished the atlas greation and added metabolic connectivity matrices.

--- src/matlab/createAtlasesFromAdniNormalizedImages.m
+++ src/matlab/createAtlasesFromAdniNormalizedImages.m
@@ -11,11 +11,11 @@ addpath(genpath([dataPartitionPath 'UNSAM/Brain/DPABI_V6.2_220915/']))
 addpath([dataPartitionPath 'UNSAM/Brain/spm12/spm12/'])
 addpath([dataPartitionPath 'UNSAM/Brain/DPABI_V6.2_220915/DPARSF/'])
 
-/home/martin/data_imaging/ADNIdata/FDG/ADNI2_FDG_T1/Processed/FinalImages/002_S_5018/
 %% PATHS AND FILENAMES
 adniPath = [adniPartitionPath '/ADNIdata/FDG/ADNI2_FDG_T1/'];
 adniDicomPath = [adniPath '/ADNI/'];
 adniProcessedPath = [adniPath '/Processed/'];
+adniUnsamPetQuantToolsPath = [adniProcessedPath '/FinalImages/'];
 normalizedDataSubfolder = 'ANTs';
 
 outputPath = [adniPath '/Atlases/'];
@@ -25,60 +25,134 @@ end
 
 % ADNI DATABASE COLLECTION
 filenameADNICollection = 'ADNI2_FDG_T1_6_05_2023.csv';
-petNormalizedFilename = 'PET_Norm_MNI_152_ANT.nii.gz';
-t1NormalizedFilename = 'T1_Norm_MNI_152_ANT1Warp.nii.gz';
-segNormalizedFilename = 'Aseg_Norm_MNI_152_ANT.nii.gz';
+adniMergeFullFilename = '/home/martin/data_imaging/ADNIdata/StudyInfo/Study_Info/ADNIMERGE_12Jul2023.csv';
+petNormalizedFilenameSuffix = 'PET_intensity_normalization_';
+t1NormalizedFilenameSuffix = 'T1_Norm_MNI_152_';
+petQuantificationValuesFilenameSuffix = 'CSV_data_';
+%segNormalizedFilename = 'Aseg_Norm_MNI_152_ANT.nii.gz';
 format = '.nii.gz';
 dcmHeadersFilename = 'dcmHeaders.mat';
 
 % Load adni dicom collection csv:
 adniCollectionData = readtable([adniPath filenameADNICollection]);
-
+adniMergeData = readtable(adniMergeFullFilename);
 %% CASES TO PROCESS
 processedSubjects = {};
 processedSubjectsPetFilenames = {};
 j = 1;
 
-dirPath = dir(adniProcessedPath);
+dirPath = dir(adniUnsamPetQuantToolsPath);
 % Go through all the cases and check if they have the images.
 for i = 3 : numel(dirPath)
-    filenamePETNormalized = fullfile(adniProcessedPath, dirPath(i).name, normalizedDataSubfolder, petNormalizedFilename);
+    filenamePETNormalized = fullfile(adniUnsamPetQuantToolsPath, dirPath(i).name, ...
+        [petNormalizedFilenameSuffix dirPath(i).name format]);
     if exist(filenamePETNormalized,'file')
+        % Read image
         processedSubjects{j} = dirPath(i).name;
         processedSubjectsPetFilenames{j} = filenamePETNormalized;
+        % Read csv
+        filenamePETQvalues = fullfile(adniUnsamPetQuantToolsPath, dirPath(i).name, ...
+            [petQuantificationValuesFilenameSuffix dirPath(i).name '.csv']);
+        petQValuesTable{j} = readtable(filenamePETQvalues);
+        petNormUptake(j,:) = petQValuesTable{j}.normalization;
         j = j + 1;
     end
 end
 %% CREATE ATLAS FOR CN
-atlasGroup = 'CN';
-indexAtlas = 0;
-% Go through all subjects and check if they are CN:
-for i = 1 : numel(processedSubjects)
-    indexMatchedName = find((strcmpi(processedSubjects{i}, adniCollectionData.Subject)) > 0); 
-    if isempty(indexMatchedName)
-        warning(sprintf('Case % not found in the database CSV.', processedSubjects{i}));
+atlasGroups = {'CN', 'AD'};
+for g = 1 : numel(atlasGroups)
+    indexAtlas = 0;
+    indicesAdniCsvSubjectsPerGroup{g} = [];
+    indicesSubjectsPerGroup{g} = [];
+    clear petImage;
+    % Go through all subjects and check if they are CN:
+    for i = 1 : numel(processedSubjects)
+        indexMatchedName = find((strcmpi(processedSubjects{i}, adniCollectionData.Subject)) > 0); 
+        indexMatchedNameMerge = find((strcmpi(processedSubjects{i}, adniMergeData.PTID)) > 0); 
+        if isempty(indexMatchedName) && isempty(indexMatchedNameMerge)
+            warning(sprintf('Case % not found in the database CSV.', processedSubjects{i}));
+        else
+            
+            if isempty(indexMatchedName)
+                if numel(indexMatchedNameMerge) > 1
+                    indexMatchedNameMerge = indexMatchedNameMerge(1); % We are looking for the initial visit.
+                end
+                groupThisSubject = adniMergeData.DX_bl{indexMatchedNameMerge};
+            else
+                if numel(indexMatchedName) > 1
+                    indexMatchedName = indexMatchedName(1); % We are looking for the initial visit.
+                end
+                groupThisSubject = adniCollectionData.Group(indexMatchedName);
+            end
+            % Is this group:
+            if strcmp(groupThisSubject, atlasGroups{g})
+                indexAtlas = indexAtlas + 1;
+                % Save the indices for later.
+                indicesAdniCsvSubjectsPerGroup{g} = [indicesAdniCsvSubjectsPerGroup{g} indexMatchedName];
+                indicesSubjectsPerGroup{g} = [indicesSubjectsPerGroup{g} i];
+                % Store the data for this subject.
+                petImage(:,:,:,indexAtlas) = niftiread(processedSubjectsPetFilenames{i});
+                subjectName{g}{indexAtlas} = processedSubjects{i};
+                group{g}{indexAtlas} = groupThisSubject;
+                if ~isempty(indexMatchedName)
+                    age_years(indexAtlas, g) = adniCollectionData.Age(indexMatchedName);
+                    sex(indexAtlas, g) = adniCollectionData.Sex{indexMatchedName};
+                    visit{g}{indexAtlas} = adniCollectionData.Visit{indexMatchedName};
+                    date(indexAtlas, g) = adniCollectionData.AcqDate(indexMatchedName);   
+                end
+            end
+            
+        end
     end
-    if numel(indexMatchedName) > 1
-        indexMatchedName = indexMatchedName(1); % We are looking for the initial visit.
-    end
-    % Is this group:
-    if strcmp(adniCollectionData.Group(indexMatchedName), atlasGroup)
-        indexAtlas = indexAtlas + 1;
-        petImage(:,:,:,indexAtlas) = niftiread(processedSubjectsPetFilenames{i});
-        subjectName{indexAtlas} = adniCollectionData.Subject{indexMatchedName};
-        group{indexAtlas} = adniCollectionData.Group{indexMatchedName};
-        age_years(indexAtlas) = adniCollectionData.Age(indexMatchedName);
-        sex(indexAtlas) = adniCollectionData.Sex{indexMatchedName};
-        visit{indexAtlas} = adniCollectionData.Visit{indexMatchedName};
-        date(indexAtlas) = adniCollectionData.AcqDate(indexMatchedName);
-    end
-end
-%%
-% Get the mean value per image:
-meanValueImages = permute(mean(mean(mean(petImage))), [4 3 2 1]);
-figure; bar(meanValueImages);
-petAtlas = mean(petImage,4);
-stdAtlas = std(petImage,4);
+    % Get the mean value per image:
+    meanValueImages = permute(mean(mean(mean(petImage))), [4 3 2 1]);
+    figure; bar(meanValueImages);
+    petAtlas{g} = mean(petImage,4);
+    stdAtlas{g} = std(petImage,[],4);
+    covAtlas{g} = stdAtlas{g}./(petAtlas{g}+1e-20);
+    % Show a slice of the atlas:
+    figure;
+    subplot(1,3,1); imshow(petAtlas{g}(:,:,round(size(petAtlas{g},3)/2))',[]);
+    subplot(1,3,2); imshow(stdAtlas{g}(:,:,round(size(petAtlas{g},3)/2))',[]);
+    subplot(1,3,3); imshow(covAtlas{g}(:,:,round(size(petAtlas{g},3)/2))',[0 max(covAtlas{g}, [], 'all')*0.2]);
+    % Write the images:
+    info = niftiinfo(processedSubjectsPetFilenames{1});
+    niftiwrite(petAtlas{g}, [outputPath 'AtlasPETFDG_' atlasGroups{g}], info);
+    niftiwrite(stdAtlas{g}, [outputPath 'AtlasStdPETFDG_' atlasGroups{g}], info);
+    niftiwrite(covAtlas{g}, [outputPath 'AtlasCovPETFDG_' atlasGroups{g}], info);
 
-% Write the images:
-info = niftiinfo(processedSubjectsPetFilenames{1});
+    % Connectvity Matrices
+    petNormUptakeValuesPerGroup{g} = petNormUptake(indicesSubjectsPerGroup{g}, :);
+    metabolicConnMatrix{g} = corr(petNormUptakeValuesPerGroup{g});
+end
+%% SHOW CONNECTIVITY METABOLIC MATRICES
+figure;
+for g = 1 : numel(atlasGroups)
+    subplot(1,numel(atlasGroups),g); imagesc(metabolicConnMatrix{g}); 
+    set(gca,'dataAspectRatio',[1 1 1])
+    title(atlasGroups{g})
+    colorbar
+end
+%% WITH ANOTHER COLORBAR
+figure;
+for g = 1 : numel(atlasGroups)
+    subplot(1,numel(atlasGroups),g); imshow(metabolicConnMatrix{g}, [0 1]); 
+    colormap(jet)
+    set(gca,'dataAspectRatio',[1 1 1])
+    title(atlasGroups{g})
+    colorbar
+end
+%% Inter-hemispheric connectivity
+indicesLeftRois = find(strcmp(petQValuesTable{1}.hemisphere, 'L')>0);
+indicesRightRois = find(strcmp(petQValuesTable{1}.hemisphere, 'R')>0);
+% Correlation right and left matrices:
+figure;
+for g = 1 : numel(atlasGroups)
+    metabolicLeftRightConnMatrix{g} = corr(petNormUptake(indicesSubjectsPerGroup{g}, indicesLeftRois), ...
+        petNormUptake(indicesSubjectsPerGroup{g}, indicesRightRois));
+    subplot(1,numel(atlasGroups),g); imshow(metabolicLeftRightConnMatrix{g}, [0 1]); 
+    colormap(jet)
+    set(gca,'dataAspectRatio',[1 1 1])
+    title(atlasGroups{g})
+    colorbar
+end
