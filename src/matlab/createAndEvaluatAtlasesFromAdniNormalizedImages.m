a4b4389d9b43827aece8030ace6a4a7a55497644
Martin Belzunce <martin.a.belzunce@gmail.com>
2023-08-14 18:32:03 -0300

    Added a code to eavluate an AD predictor with a leave one out scheme.

--- src/matlab/createAndEvaluatAtlasesFromAdniNormalizedImages.m
+++ src/matlab/createAndEvaluatAtlasesFromAdniNormalizedImages.m
@@ -62,13 +62,13 @@ for i = 3 : numel(dirPath)
         j = j + 1;
     end
 end
-%% CREATE ATLAS FOR CN
+%% CREATE ATLAS FOR CN AND EVALUATE USING LEAVE ONE OUT
 atlasGroups = {'CN', 'AD'};
+% First create atlases for both groups.
 for g = 1 : numel(atlasGroups)
     indexAtlas = 0;
     indicesAdniCsvSubjectsPerGroup{g} = [];
     indicesSubjectsPerGroup{g} = [];
-    clear petImage;
     % Go through all subjects and check if they are CN:
     for i = 1 : numel(processedSubjects)
         indexMatchedName = find((strcmpi(processedSubjects{i}, adniCollectionData.Subject)) > 0); 
@@ -86,18 +86,19 @@ for g = 1 : numel(atlasGroups)
                 if numel(indexMatchedName) > 1
                     indexMatchedName = indexMatchedName(1); % We are looking for the initial visit.
                 end
-                groupThisSubject = adniCollectionData.Group(indexMatchedName);
+                groupThisSubject = adniCollectionData.Group{indexMatchedName};
             end
             % Is this group:
             if strcmp(groupThisSubject, atlasGroups{g})
                 indexAtlas = indexAtlas + 1;
                 % Save the indices for later.
+                group{i} = groupThisSubject;
                 indicesAdniCsvSubjectsPerGroup{g} = [indicesAdniCsvSubjectsPerGroup{g} indexMatchedName];
                 indicesSubjectsPerGroup{g} = [indicesSubjectsPerGroup{g} i];
                 % Store the data for this subject.
-                petImage(:,:,:,indexAtlas) = niftiread(processedSubjectsPetFilenames{i});
+                petImages{g}(:,:,:,indexAtlas) = niftiread(processedSubjectsPetFilenames{i});
                 subjectName{g}{indexAtlas} = processedSubjects{i};
-                group{g}{indexAtlas} = groupThisSubject;
+                %group{g}{indexAtlas} = groupThisSubject;
                 if ~isempty(indexMatchedName)
                     age_years{g}(indexAtlas) = adniCollectionData.Age(indexMatchedName);
                     sex{g}(indexAtlas) = adniCollectionData.Sex{indexMatchedName};
@@ -114,10 +115,10 @@ for g = 1 : numel(atlasGroups)
         end
     end
     % Get the mean value per image:
-    meanValueImages = permute(mean(mean(mean(petImage))), [4 3 2 1]);
+    meanValueImages = permute(mean(mean(mean(petImages{g}))), [4 3 2 1]);
     figure; bar(meanValueImages);
-    petAtlas{g} = mean(petImage,4);
-    stdAtlas{g} = std(petImage,[],4);
+    petAtlas{g} = mean(petImages{g},4);
+    stdAtlas{g} = std(petImages{g},[],4);
     covAtlas{g} = stdAtlas{g}./(petAtlas{g}+1e-20);
     % Show a slice of the atlas:
     figure;
@@ -134,11 +135,82 @@ for g = 1 : numel(atlasGroups)
     petNormUptakeValuesPerGroup{g} = petNormUptake(indicesSubjectsPerGroup{g}, :);
     metabolicConnMatrix{g} = corr(petNormUptakeValuesPerGroup{g});
 end
+%% CREATE INDICES FOR THE GROUP VARIABLE
+groupsIndex = [];
+for g = 1 : numel(atlasGroups)
+    groupsIndex(strcmp(group, atlasGroups{g})) = g;
+end
+%% ATLAS DISCRDING OUTLIERS
+thresholdNumOutlierRegions = 10;
+for g = 1 : numel(atlasGroups)
+    outliersPerGroupAndRegions{g} = isoutlier(petNormUptakeValuesPerGroup{g});
+    % Now check how many regions:
+    numOutlierRegions = sum(outliersPerGroupAndRegions{g},2);
+    % Check how many surpass the threshold:
+    indicesOutliers{g} = numOutlierRegions > thresholdNumOutlierRegions;
+    numOutliers{g} = sum(indicesOutliers{g});
+    % Create atlas without outliers:
+    petAtlas{g} = mean(petImages{g}(:,:,:,~indicesOutliers{g}), 4);
+    stdAtlas{g} = std(petImages{g}(:,:,:,~indicesOutliers{g}),[],4);
+    covAtlas{g} = stdAtlas{g}./(petAtlas{g}+1e-20);
+    niftiwrite(petAtlas{g}, [outputPath 'AtlasPETFDG_FiltOutliers_' atlasGroups{g}], info);
+    niftiwrite(stdAtlas{g}, [outputPath 'AtlasStdPETFDG_FiltOutliers_' atlasGroups{g}], info);
+    niftiwrite(covAtlas{g}, [outputPath 'AtlasCovPETFDG_FiltOutliers_' atlasGroups{g}], info);
 
+    fprintf('Number of outliers exluded in the %s atlas: %d.\n', atlasGroups{g}, numOutliers{g});
+end
+%% SHOW ATLASES
+% Create a figure with both atlases:
+bothAtlases = [flip(permute(petAtlas{1}, [2 1 3]),1), flip(permute(petAtlas{2}, [2 1 3]),1)];
+figure;
+imshow(bothAtlases(:,:, size(petAtlas{1},3)/2-5),[]);
+colormap('turbo');
+colorbar
+set(gcf, 'Position', [100 100 1600 1200]);
+for g = 1 :numel(atlasGroups)
+    y = 15;
+    x = (g-1)*size(petAtlas{1},1) + size(petAtlas{1},1)/2-12;
+    text(x, y, atlasGroups{g}, 'FontSize', 18, 'FontWeight', 'bold','Color', 'w');
+end
+%% USE THE ATLASES TO CLASSIFY ALZHEIMERS WITH A LEAVE ONE OUT SCHEME
+% Go throough each case, check the group and create a new atlas if this
+% subejcts is included in the atlas.
+for i = 1 : size(petNormUptake,1)
+    % Save the original atlases:
+    for g = 1 : numel(atlasGroups)
+        petAtlasLeaveOneOut{g} = petAtlas{g};
+    end
+    % Get the group for this subject:
+    groupIndexThisSubject = groupsIndex(i);
+    % Index in subgroup:
+    indexInGroup = find((indicesSubjectsPerGroup{groupIndexThisSubject} == i) > 0);
+    % Generate an atlas without this case, if this subjects is included
+    % in the atlas (not an outlier).
+    if indicesOutliers{groupIndexThisSubject}(indexInGroup) == 0
+        indicesForAtlas = ~indicesOutliers{groupIndexThisSubject};
+        indicesForAtlas(indexInGroup) = 0;
+        % Update this atlas
+        petAtlasLeaveOneOut{groupIndexThisSubject} = mean(petImages{groupIndexThisSubject}(:,:,:,indicesForAtlas),4);
+    end
+    % Compute distance to each atlas:
+    petImageThisSubject = petImages{groupIndexThisSubject}(:,:,:, indexInGroup);
+    for g = 1 : numel(atlasGroups)
+        ceAtlas(i,g) = crossentropy(petImageThisSubject, petAtlas{g});
+        frobeniusDistanceAtlas(i,g) = norm(petImageThisSubject -  petAtlas{g}, 'fro');
+    end
+end
+% Classify each subject based on the frobenius distance:
+[minFroDistAtlas, indMinFroDistAtlas] = min(frobeniusDistanceAtlas, [],2);
+% Check accuracy of the predictor.
+confMatrix = confusionmat(groupsIndex, indMinFroDistAtlas); % Groups index: ground truth, indMinFroDistAtlas: prediction
+accuracyAtlasPredictor = (confMatrix(1,1)+confMatrix(2,2))./sum(confMatrix(:))*100;
+%% SHOW THE RESULTS IN A CONFUSION MATRIX
+figure
+cm = confusionchart(groupsIndex,indMinFroDistAtlas);
 %% DIFFERENCE BETWEEN ATLASES
 % Diff atlases:
 diffAtlas = (petAtlas{2}-petAtlas{1})./petAtlas{1}*100;
-figure; imshow(diffAtlas(:,:,120),[-50 50])
+figure; imshow(diffAtlas(:,:,80),[-50 50])
 %% SHOW CONNECTIVITY METABOLIC MATRICES
 figure;
 for g = 1 : numel(atlasGroups)
@@ -146,18 +218,6 @@ for g = 1 : numel(atlasGroups)
     set(gca,'dataAspectRatio',[1 1 1])
     title(atlasGroups{g})
     colorbar
-end
-%% SAVE CONNECTIVITY MATRICES
-for g = 1 : numel(atlasGroups)
-    figure;
-    imshow(metabolicConnMatrix{g}, [0 1]); 
-    colormap(jet);
-    set(gca,'dataAspectRatio',[1 1 1])
-    %title(atlasGroups{g})
-    colorbar
-    set(gcf, 'Position', [100 100 2000 2000])
-    saveas(gca, [outputPath sprintf('ConnectivityMatrix_%s.png', atlasGroups{g})])
-    exportgraphics(gca, [outputPath sprintf('ConnectivityMatrix_%s.png', atlasGroups{g})])
 end
 %% WITH ANOTHER COLORBAR
 figure;
@@ -168,134 +228,12 @@ for g = 1 : numel(atlasGroups)
     title(atlasGroups{g})
     colorbar
 end
-%% Inter-hemispheric connectivity
-indicesLeftRois = find(strcmp(petQValuesTable{1}.hemisphere(petQValuesTable{1}.n_label~=0), 'L')>0);
-indicesRightRois = find(strcmp(petQValuesTable{1}.hemisphere(petQValuesTable{1}.n_label~=0), 'R')>0);
-% Correlation right and left matrices:
+%% WITH ANOTHER COLORBAR
 figure;
 for g = 1 : numel(atlasGroups)
-    metabolicLeftRightConnMatrix{g} = corr(petNormUptake(indicesSubjectsPerGroup{g}, indicesLeftRois), ...
-        petNormUptake(indicesSubjectsPerGroup{g}, indicesRightRois));
-    subplot(1,numel(atlasGroups),g); imshow(metabolicLeftRightConnMatrix{g}, [0 1]); 
+    subplot(1,numel(atlasGroups),g); imshow(metabolicConnMatrix{g}, [0 1]); 
     colormap(jet)
     set(gca,'dataAspectRatio',[1 1 1])
     title(atlasGroups{g})
     colorbar
 end
-%% USE THE MAP TO CLASSIFY ALZHEIMERS
-for i = 1 : size(petNormUptake,1)
-    petImageThisSubject= niftiread(processedSubjectsPetFilenames{i});
-    matrixPerSubject = petNormUptake(i,:)' * petNormUptake(i,:);
-%     subplot(1,numel(atlasGroups)+1,1); imshow(matrixPerSubject, [0 1]); 
-%     colormap(jet);
-%     for g = 1 : numel(atlasGroups)
-%         subplot(1,numel(atlasGroups)+1,g+1); imshow(metabolicConnMatrix{g}, [0 1]); 
-%         colormap(jet);
-%     end
-%     pause;
-    % Compute distance to each matrix:
-    for g = 1 : numel(atlasGroups)
-        ce(i,g) = crossentropy(matrixPerSubject, metabolicConnMatrix{g});
-        ecuDistance(i,g) = norm(matrixPerSubject -  metabolicConnMatrix{g});
-        frobeniusDistance(i,g) = norm(matrixPerSubject -  metabolicConnMatrix{g}, 'fro');
-    end
-    % Compute distance to each atlas:
-    for g = 1 : numel(atlasGroups)
-        ceAtlas(i,g) = crossentropy(petImageThisSubject, petAtlas{g});
-        frobeniusDistanceAtlas(i,g) = norm(petImageThisSubject -  petAtlas{g}, 'fro');
-    end
-end
-%%
-[minFroDistAtlas, indMinFroDistAtlas] = min(frobeniusDistanceAtlas, [],2);
-%%
-classes = cell(size(petNormUptake, 1),1);
-% Binary label of AD
-indexGroupAD = strcmp(atlasGroups, 'AD');
-for g = 1 : numel(indexGroupAD)
-    for i = 1 : numel(indicesSubjectsPerGroup{g})
-        classes{indicesSubjectsPerGroup{g}(i)} = atlasGroups{g};
-    end
-end
-%%
-predictionAtlases = cell(size(petNormUptake, 1),1);
-for g = 1 : numel(atlasGroups)
-    indClass = find(indMinFroDistAtlas == g > 0);
-    for i = 1 : numel(indClass)
-        predictionAtlases{indClass(i)} = atlasGroups{g};
-    end
-end
-
-ind = strcmp(predictionAtlases, classes);
-predictionError = sum(ind == 0)./numel(ind);
-%% CLASSIFIER BASED IN A SVM
-classes = cell(size(petNormUptake, 1),1);
-% Binary label of AD
-indexGroupAD = strcmp(atlasGroups, 'AD');
-for g = 1 : numel(indexGroupAD)
-    for i = 1 : numel(indicesSubjectsPerGroup{g})
-        classes{indicesSubjectsPerGroup{g}(i)} = atlasGroups{g};
-    end
-end
-SVMModel = fitcsvm(petNormUptake,classes);
-
-sv = SVMModel.SupportVectors;
-figure
-gscatter(petNormUptake(:,1),petNormUptake(:,2),classes)
-hold on
-plot(sv(:,1),sv(:,2),'ko','MarkerSize',10)
-legend('CN','AD','Support Vector')
-hold off
-%% PET NORM UPTAKE DIFFERENCES
-for g = 1 : numel(atlasGroups)
-    meanUptakePerRegionAndGroup(g,:) = mean(petNormUptakeValuesPerGroup{g});
-end
-% Get the difference:
-diffUptakePerGroup = [meanUptakePerRegionAndGroup(2,:) - meanUptakePerRegionAndGroup(1,:)];
-
-% BOXPLOT
-figure;
-set(gcf, 'Position', [100 100 4000 2400]);
-%ha = boxchart(ax1, nameSingleMuscle, ...
-%volValues_cm3(namedMuscle == labelsMuscle(i)),'GroupByColor',namedGroup(namedMuscle == labelsMuscle(i)), 'Notch', 'on');
-regionsIds = repmat(1:size(petNormUptake,2), size(petNormUptake,1), 1);
-vecRegionsIds = vec(regionsIds);
-labelNames = labelNamesHammersAtlas.structure(roiIds);
-vecLabelNames = categorical(vec(labelNames));
-vecPetNormUptake = vec(petNormUptake);
-vecClasses = vec(repmat(classes, 1, size(petNormUptake,2)));
-boxchart(vecLabelNames, vecPetNormUptake, 'GroupByColor', vecClasses);
-%%
-% Estimate signiicant differences with kruskalwallis:
-for i = 1 : size(petNormUptake,2)
-    p(i) = kruskalwallis(petNormUptake(:,i),classes,'off');
-end
-%%
-for i = 1 : size(petNormUptake,2)
-    [h_ttest(i), p_ttest(i)] = ttest2(petNormUptakeValuesPerGroup{1}(:,i), petNormUptakeValuesPerGroup{2}(:,i));
-end
-% Names of the regions that are different:
-disp(labelNames(1,h_ttest==1))
-%%
-SVMModel = fitcsvm(petNormUptake,classes,'Standardize',true,'KernelFunction','RBF',...
-    'KernelScale','auto');
-CVSVMModel = crossval(SVMModel);
-classLoss = kfoldLoss(CVSVMModel);
-%%
-svInd = SVMModel.IsSupportVector;
-h = 0.02; % Mesh grid step size
-[X1,X2] = meshgrid(min(petNormUptake(:,1)):h:max(petNormUptake(:,1)),...
-    min(petNormUptake(:,2)):h:max(petNormUptake(:,2)));
-[~,score] = predict(SVMModel,[petNormUptake(:),petNormUptake(:)]);
-scoreGrid = reshape(score,size(petNormUptake,1),size(petNormUptake,2));
-
-figure
-plot(petNormUptake(:,1),petNormUptake(:,2),'k.')
-hold on
-plot(petNormUptake(svInd,1),petNormUptake(svInd,2),'ro','MarkerSize',10)
-contour(X1,X2,scoreGrid)
-colorbar;
-title('{\bf Iris Outlier Detection via One-Class SVM}')
-xlabel('Sepal Length (cm)')
-ylabel('Sepal Width (cm)')
-legend('Observation','Support Vector')
-hold off
